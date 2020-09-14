19ed4f5895f4b5611ec4346bc2f9d25e51462595
Stewart Greenhill <stewart.greenhill@gmail.com>
2020-09-14 23:20:29 +0800

    Updated bootstrap script

--- script/build-omake.sh
+++ script/build-omake.sh
@@ -12,5 +12,5 @@ Ofront+ -88 -s -e -2 src/aria/arPattern.ob2
 Ofront+ -88 -s -e -2 src/aria/arSize.ob2
 Ofront+ -88 -s -e -2 src/aria/arConfiguration.ob2
 Ofront+ -88 -s -e -2 src/aria/arPath.ob2
-Ofront+ -88 -s -e -2 -m tool/OMake.ob2
+Ofront+ -88 -s -e -2 -m src/tool/OMake.ob2
 gcc -O3 -g3 -I$OFRONT/Mod/Lib -I$OFRONT/Target/$OTARGET/Lib/Obj -L$OFRONT/Target/$OTARGET/Lib arPath.c arSize.c arStream.c arOut.c arChar.c arC.c arCFormat.c arText.c arPattern.c arConfiguration.c arStringList.c arStringAssoc.c arStrings.c OMake.c -o OMake -lOfront 
